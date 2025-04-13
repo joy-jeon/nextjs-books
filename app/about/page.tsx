ecc41af36f4a5f5a90ba18ad0afc7001936ddb88
joy-jeon <ha0913hyun@hanmail.net>
2025-04-13 18:38:32 +0900

    react import 추가한 초기파일

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const metadata = {
+  title: "About us",
+};
+
+export default function About() {
+  return <h1>About us</h1>;
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Tomato() {
+  return <h1> Next</h1>;
+}
