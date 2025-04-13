696678ab71644f1e12570e95e311f3f794262c58
joy-jeon <ha0913hyun@hanmail.net>
2025-04-14 04:13:22 +0900

    Books Dynamic Routes 변경

--- app/(books)/books/[id]/page.tsx
+++ app/(books)/books/[id]/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Books() {
-  return <h1>책이다</h1>;
+export default function Books({ params: { id } }: { params: { id: number } }) {
+  return <h1>책이다{id}</h1>;
 }
